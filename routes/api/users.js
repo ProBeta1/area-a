139519031e87e12ab8ec74d1295b820dae9b2bd6
ProBeta1 <abhinavkishan.56@gmail.com>
2020-03-16 19:24:44 +0530

    Register API completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
+const gravatar = require("gravatar");
+//Load user model
+const User = require("../../models/User");
 
 // @route GET api/users/test
 // @desc Checks users route
@@ -10,5 +12,35 @@ router.get("/test", (req, res) =>
     msg: " stuff of users"
   })
 );
+
+// @route GET api/users/register
+// @desc register a new user
+// @access Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email is already registered!" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // size of icon
+        r: "pg", //idk , it says rating
+        d: "retro" // default icon
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password // Wont hash your pass , do whatever you can!
+      });
+
+      newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+    }
+  });
+});
 
 module.exports = router;
