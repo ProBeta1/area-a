780bcdec4d737ad2cbf038d381b04072a1851603
ProBeta1 <abhinavkishan.56@gmail.com>
2020-03-16 19:40:37 +0530

    Login API done

--- routes/api/users.js
+++ routes/api/users.js
@@ -43,4 +43,26 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route GET api/users/login
+// @desc Logging in a user / return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find the user through email
+  User.findOne({ email }).then(user => {
+    if (user) {
+      //check the pass , remember , no encyption
+      if (password === user.password) {
+        return res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password is incorrect!" });
+      }
+    } else {
+      return res.status(404).json({ email: "User not found" });
+    }
+  });
+});
+
 module.exports = router;
